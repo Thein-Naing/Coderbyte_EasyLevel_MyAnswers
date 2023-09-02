5d510f1721cc3d872f18beae2eeb940e924dffb5
Thein Naing <theinnaing0908@gmail.com>
2023-09-02 20:21:14 +0800

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -5,7 +5,7 @@ Have the function Palindrome(str) take the str parameter being passed and
  (the string is the same forward as it is backward) otherwise return the string false. 
  For example: "racecar" is also "racecar" backwards.
  Punctuation and numbers will not be part of the string.
-  Difficulty Level : Easy
+  `Difficulty Level : Easy`
 
 
 
