a1b5dcf1b22fbbbbb047a70182c27207f2e15347
Thein Naing <theinnaing0908@gmail.com>
2023-09-02 20:10:24 +0800

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -5,8 +5,8 @@
 
 //4.loop through given word 
  for (let i = 0; i < word.length; i++) {
-//5. add each letter of word into letters array. letters.push(word[i]);
-//6. Again pop each letter of word into letters array in reverse order. reverseWord += letters.pop();
+//5. add each letter of word into letters array. // letters.push(word[i]);
+//6. Again pop each letter of word into letters array in reverse order. // reverseWord += letters.pop();
  reverseWord += letters.pop(letters.push(word[i])); //7. reverseWord = reverseWord + letters.pop(letters.push(word[i]));
  }
  
