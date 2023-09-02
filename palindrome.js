a8df2b50b22803c64b7573b79a5a514d66f22b69
Thein Naing <theinnaing0908@gmail.com>
2023-09-02 20:19:25 +0800

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,4 +1,15 @@
- let letters = []; //1.define an empty array to store each letter of given word.
+Palindrome
+HIDE QUESTION
+Have the function Palindrome(str) take the str parameter being passed and 
+ return the string true if the parameter is a palindrome, 
+ (the string is the same forward as it is backward) otherwise return the string false. 
+ For example: "racecar" is also "racecar" backwards.
+ Punctuation and numbers will not be part of the string.
+
+
+
+
+let letters = []; //1.define an empty array to store each letter of given word.
  let word = "racecar"; //2.declare given word to check.
  let reverseWord = '';  //3.define reverseWord variable as a empty string initially.
 
