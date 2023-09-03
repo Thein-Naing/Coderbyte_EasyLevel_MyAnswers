3685450f9f6ad48c42c75c4a77df13ed35bd97b3
Thein Naing <theinnaing0908@gmail.com>
2023-09-03 15:26:16 +0800

    Update QuestionsMarks.js

--- QuestionsMarks.js
+++ QuestionsMarks.js
@@ -18,7 +18,7 @@ for ( let i = 0; i < str.length; i++) { //2A. loop first number with index i and
   for (let j = i+1; j < str.length; j++) { //2B. inside first number loop, loop the second number with index j with initial value i+1.
     if(Number(str[i]) + Number(str[j]) === 10){ //2C. check number value of indxes i + j === 10, if say yes then result is true.
       res = true;
-      //3.Now we have to check quantity of ? is exactly3 between index i and j.
+      //3.Now we have to check quantity of ? is exactly 3 between index i and j.
       //3A.slice index i and index j from i???j or i??j first and ??? or ?? left.
       //3B.Then split the ? and then find length of ?  and check with quantity of ?
       if(str.slice(i,j).split("?").length -1 < 3) { 
