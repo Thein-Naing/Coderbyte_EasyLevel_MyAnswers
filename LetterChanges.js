874574b658c6eb089e850ee12d82963fd2cb38ee
Thein Naing <theinnaing0908@gmail.com>
2023-09-03 14:22:34 +0800

    Update LetterChanges.js

--- LetterChanges.js
+++ LetterChanges.js
@@ -10,35 +10,36 @@ Output: gvO Ujnft! */
 
 function LetterChanges(str) { 
 
-  // code goes here 
-  /*1. define a str1 by using regex replace method with two argument: (1) regex for charCode a to y 
-  with global flag g and i & (2) function with argument(a) which return  
-  replacing "a" letter in the string with the letter following it in the alphabet 
-  by using String.fromCharCode method and charCodrAt method...
+  // code goes here  
 
-The String.fromCharCode() method converts Unicode values to characters.
-The String.fromCharCode() is a static method of the String object.
-The syntax is always String.fromCharCode().
+  //1. create three strings; alphatStr for alphabet letters, newStr for modified alphatStr and finalStr for solution.
+    let alphatStr = "abcdefghijklmnopqrstuvwxyz";
+    let newStr = "bcdEfghIjklmnOpqrstUvwxyzA";
+    let finalStr = "";
 
-The charCodeAt() method returns the Unicode of the character at a specified index (position) in a string.
-The index of the first character is 0, the second is 1, ....
-The index of the last character is string length - 1 */
-
-  //2A. we convert (Unicode value of ) letter "a" to (Unicode value of ) letter "b" by charCodeAt method ======> a.charCodeAt(0) + 1.
-  //2B. then we covert (Unicode value of) letter "a" to character "a" by using String.fromCharCode method =====> String.fromCharCode(a.charCodeAt(0)+1)}) and return.
+  //2. loop through alphatStr and check the index of str[i] and return finalStr .
+  /* 3.The charAt() method returns the character at a specified index (position) in a string.
+      The index of the first character is 0, the second 1, and so on ... */
   
-  str1 = str.replace(/[a-y]/gi, function(a){return String.fromCharCode(a.charCodeAt(0)+1)});
-
-  //3. create str2 using regex replace method by replacing "z" with "a" from str1.
-  str2 = str1.replace(/z/gi,"a");
-
-  //4. then create str3 using regex replace method by replacing vowels by a function call with returning UpperCase vowels from str2.
-  str3 = str2.replace(/a|e|i|o|u/gi, function(a){return a.toUpperCase()});
-
-  //5.return str3;
-  return str3; 
-         
+    for (i = 0; i < str.length; i++) {
+    
+  //4. find the index of str[1] and if it is !== -1, then  =====>  finalStr = finalStr + newStr.charAt(alphatStr.indexOf(str[i]));
+      
+        if (alphatStr.indexOf(str[i]) !== -1) { 
+          
+ //5. e.g. alphatStr.indexOf(str[b]) = 1; then =====>  finalStr = finalStr + "bcdEfghIjklmnOpqrstUvwxyzA".charAt(1);
+          
+          finalStr += newStr.charAt(alphatStr.indexOf(str[i]));
+          
+        } else {
+            finalStr += str[i]; 
+    }
+  //6. return finalStr;
+  
+    return finalStr;
 }
    
 // keep this function call here 
 LetterChanges(readline());
+
+   
