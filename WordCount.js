b756aac1e867678770eccf4492820dabe29d7541
Thein Naing <theinnaing0908@gmail.com>
2023-09-03 11:01:59 +0800

    Update WordCount.js

--- WordCount.js
+++ WordCount.js
@@ -1,7 +1,8 @@
+/* WordCount 
 Have the function WordCount(str) take the str string parameter being passed 
 and return the number of words the string contains 
 (e.g. "Never eat shredded wheat or cake" would return 6).
-Words will be separated by single spaces.
+Words will be separated by single spaces. */
 
 function WordCount(str) { 
 
