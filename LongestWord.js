ef19b101314113036df5ee88347911fb5258deb8
Thein Naing <theinnaing0908@gmail.com>
2023-09-03 12:09:18 +0800

    Update LongestWord.js

--- LongestWord.js
+++ LongestWord.js
@@ -6,13 +6,17 @@ Ignore punctuation and assume sen will not be empty. Words may also contain numb
 
 function LongestWord(sen) { 
 
+  //1. use regex string.match method to return only characters a through z and 0 through 9, stripping away punctuation.
 let arr = sen.match(/[a-z]+/gi);
 
+
+  //2. sort the arr with a function(a, b) parameter which will compare each element with next element.
 sortedArr = arr.sort(function((a, b) => {
   b.length > a.length 
 });
 
-retirn sortedArr[0];
+  //3. Assuming there are two or more words that are the same length, return the first word from the string with that length.
+return sortedArr[0];
 
 // keep this function call here 
 LongestWord(readline());
