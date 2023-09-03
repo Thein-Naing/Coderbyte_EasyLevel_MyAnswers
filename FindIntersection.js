711e0f9c58324c8c230dee6bff3d114126b97dfc
Thein Naing <theinnaing0908@gmail.com>
2023-09-03 11:23:51 +0800

    Update FindIntersection.js

--- FindIntersection.js
+++ FindIntersection.js
@@ -6,15 +6,19 @@ Your goal is to return a comma-separated string containing the numbers that occu
 If there is no intersection, return the string false.*/
 
 
-
 function FindIntersection(strArr) { 
 
-  let a = strArr[0].split(', ')
-  let b = strArr[1].split(', ')
-  let result = a.filter(x => b.find(a => a === x))
+  let a = strArr[0].split(', ') // 1.split the , with one space in first str for comma-separated numbers to find intersection string..
+  let b = strArr[1].split(', ') // 2. split the , with one space in second str for comma-separated numbers to find intersection string.
+  
+  //3A. filter the first string with intersection called x.
+  //3B. return this intersection x in second string with arrow function
+  //3C. find the first string intersection inside second string call
+  //3D. check returning first string intersection === intersection string x
+  //3E. If say yes, then check result.length is greater than zero and use ternary operator and .join method for intersection or false.
+  
+  let result = a.filter(x => b.find(a => a === x)) 
   return result.length > 0 ? result.join(',') : 'false'
-
 }
-   
-// keep this function call here 
+   // keep this function call here 
 console.log(FindIntersection(readline()));
