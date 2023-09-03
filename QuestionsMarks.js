12f278755985231c337a316499913da424e5da01
Thein Naing <theinnaing0908@gmail.com>
2023-09-03 11:56:01 +0800

    Update QuestionsMarks.js

--- QuestionsMarks.js
+++ QuestionsMarks.js
@@ -11,12 +11,17 @@ between 5 and 5 at the end of the string. */
 
 function QuestionsMarks(str) { 
 
-let res = false;
-for ( let i = 0; i < str.length; i++) {
-  for (let j = i+1; j < str.length; j++) {
-    if(Number(str[i]) + Number(str[j]) === 10){
+let res = false; //1. defince initial state of result === false.
+  
+   //2. It will be nested loop and we have to loop with pair of two single digit numbers(e.g.Num (i + j) === 10).
+for ( let i = 0; i < str.length; i++) { //2A. loop first number with index i and initial value 0.
+  for (let j = i+1; j < str.length; j++) { //2B. inside first number loop, loop the second number with index j with initial value i+1.
+    if(Number(str[i]) + Number(str[j]) === 10){ //2C. check number value of indxes i + j === 10, if say yes then result is true.
       res = true;
-      if(str.slice(i,j).split("?").length -1 < 3) {
+      //3.Now we have to check quantity of ? is exactly3 between index i and j.
+      //3A.slice index i and index j from i???j or i??j first and ??? or ?? left.
+      //3B.Then split the ? and then find length of ?  and check with quantity of ?
+      if(str.slice(i,j).split("?").length -1 < 3) { 
         return false;
       }
     }
