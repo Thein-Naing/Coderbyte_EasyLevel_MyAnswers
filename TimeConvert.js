ca433fa1dda2ed6f1504681b8b0d6946a46d244e
Thein Naing <theinnaing0908@gmail.com>
2023-09-03 11:01:10 +0800

    Update TimeConvert.js

--- TimeConvert.js
+++ TimeConvert.js
@@ -1,8 +1,8 @@
-Time Convert
+/ *Time Convert
 
 Have the function TimeConvert(num) take the num parameter being passed and 
 return the number of hours and minutes the parameter converts to (ie. if num = 63 then the output should be 1:3). 
-Separate the number of hours and minutes with a colon.
+Separate the number of hours and minutes with a colon. */
 
   function TimeConvert(num) { 
 
