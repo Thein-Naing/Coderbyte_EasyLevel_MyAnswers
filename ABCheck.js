a419aad1e1174ae28153620667adc937eff3e2a8
Thein Naing <theinnaing0908@gmail.com>
2023-09-06 09:02:28 +0800

    Update ABCheck.js

--- ABCheck.js
+++ ABCheck.js
@@ -17,14 +17,8 @@ function ABCheck(str) {
       i flag for ingoring lower case and upper case
       finally we end up with /a.{3}b/gi and /b.{3}a/gi
   //3. then return it by using ternary operator for true and false */
-  
-    if ((str.match(/a.{3}b/gi)) || (str.match(/b.{3}a/gi))) {
-       return true;
-   }  else {    
-       return false;   
-   }
-}
-  //4. Or more clean and clear code.
+
+  //4. use || operator together with ternary operator. 
       return ((str.match(/a.{3}b/gi)) || (str.match(/b.{3}a/gi))) ? true : false;
 
   // keep this function call here 
