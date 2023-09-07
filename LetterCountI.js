6a9901d3991dfcfa152fb8fb55c470237a339450
Thein Naing <theinnaing0908@gmail.com>
2023-09-07 14:29:00 +0800

    Update LetterCountI.js

--- LetterCountI.js
+++ LetterCountI.js
@@ -10,7 +10,8 @@ Output: -1*/
 
 
 /* Method Statement: This is a pretty typical way to solve this sort of algorithm. 
-                     It essentially runs through the for loop to find a result what we want,
+                     It essentially runs through the for loop to find a result what we want
+                     and until we found the desired result.
                      if it is not found, return a default statement (i.e. return -1;). 
                      If there is only one return statement for (for loop), 
                      then it could be slightly better for clean code.*/
@@ -21,20 +22,23 @@ let words = str.split(" ");  //1. convert given str to words.
    let count = 0;   // 2A. initial state of count shall be "zero".
    let result = -1; // 2B. assuming there are no words with repeating letters (it is given statement from question).
  
-   for(let i=0; i < words.length; i++) {//3. loop through words with index i and pass the word with index i.
+   for(let i=0; i < words.length; i++) {//3. loop through words with index i and define the word with index i of words..
      let word= words[i];
 
-     for( let j=0; j< word.length; j++) {//4. loop through word[i] with index j and pass the letter.
+     for( let j=0; j< word.length; j++) {//4.loop through word with index j and pass the letter.
        let letter = word[j];
 
-       for(let k=0; k < word.length; k++) {//5. loop through word[i] with index k and pass the  another letter.
+       for(let k=0; k < word.length; k++) {//5.loop through word with index k and pass the another letter.
       
-       /* 6.letter being pased in work[k] is not literally the same as letter being pased 
-       in work[j]  and so no words with repeating letters */
+       /* 6A.letter being pased in work[k] is not literally the same as letter being pasedin work[j]  
+       6B.and so no words with repeating letters */
+         
            if( k !== j && letter === word[k] && result === -1) {
-              count += 1 // count = count+1; 
+             
+              count += 1;  // 7. then update the state of count by =====> count = count+1 =====> count += 1;  
 
-              if(count > 1) {
+              if(count > 1) { //8. finally update the state of result if state of count is changed and return the result.
+                
                  result = word;
               
            }
