9783e3ed3eb39f8d99b05a97373d27f9e6d83416
Thein Naing <theinnaing0908@gmail.com>
2023-09-07 14:41:39 +0800

    Update LetterCountI.js

--- LetterCountI.js
+++ LetterCountI.js
@@ -20,7 +20,7 @@ Output: -1*/
 function LetterCountI(str) { 
 
 let words = str.split(" ");  //1. convert given str to words.
-   let count = 0;   // 2A. initial state of count shall be "zero".
+   let count = 0;   // 2A. initial state of count for repeated letter shall be "zero".
    let result = -1; // 2B. assuming there are no words with repeating letters (it is given statement from question).
  
    for(let i=0; i < words.length; i++) {//3. loop through words with index i and define the word with index i of words..
