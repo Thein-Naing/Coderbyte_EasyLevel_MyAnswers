27bfd7111d11fbcbd3196866bcc93a3d4a252eb7
Thein Naing <theinnaing0908@gmail.com>
2023-09-12 20:57:07 +0800

    Update DashInsert.js

--- DashInsert.js
+++ DashInsert.js
@@ -21,8 +21,8 @@ function DashInsert(str) {
   /*2.   / / === start and end of regex.
    d === digits (0-9) 
    d*[12579]  === digits [13579] any character of: '1', '3', '5', '7', '9'
-  (?  === look ahead to see if there is a match in following number
-   d*[13579] === digits [13579] any character of: '1', '3', '5', '7', '9'
+  (?  === look ahead to see if there is a match for 
+   d*[13579] === digits [13579] any character of: '1', '3', '5', '7', '9 in following number
   ) === end of look-ahead   
    g ==== global search flag
      */
@@ -30,11 +30,13 @@ function DashInsert(str) {
     /* 3. if match we found there are 2 odd numbers and so 
     we will add "-" sign betwwen this 2 odd numbers by using  $1.
     3A. $1 === $1 is a first captured group.
-     this values used in replacement patterns. 
-     this is substitutions and specifically used in numbered group substitutions. */
+     this value used in replacement patterns. 
+     this is substitution and specifically used in numbered group substitutions. */
 
      /*  4. MEANING : Hey regex! find if each and every folowoing number is 1,3,5,7,9 for current number 1,3,5,7,9.
-     if you found it then add "-" between this 2 odd number using $1 sign and return it..
+     if you found it then add "-" between this 2 odd number using $1 sign and return it.
+     
+     5. use str.replace method.
      */
     
   return str.replace(/(d*[13579])(?=d*[13579])/g, "$1-");
