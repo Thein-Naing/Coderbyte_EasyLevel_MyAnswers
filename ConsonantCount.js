4ce38f2ff261178e149b1c25613d35926bb0c86a
Thein Naing <theinnaing0908@gmail.com>
2023-09-13 13:45:56 +0800

    Update ConsonantCount.js

--- ConsonantCount.js
+++ ConsonantCount.js
@@ -6,8 +6,9 @@ Have the function ConsonantCount(str) take the str string parameter being passed
   /* 1. create and use regex
   1A. /  / start and end of regex.
   1B.[b-df-hj-np-tv-xz]are consonents
+  1C. g flag for global serch  and i flag for uppercase ,lowercase to be ignored.
    
-  2. use str.match method to find length and then return it.  */
+  2. use str.match().length method to find length and then return it.  */
    
   return str.match(/[b-df-hj-np-tv-xz]/gi).length;  
 }
