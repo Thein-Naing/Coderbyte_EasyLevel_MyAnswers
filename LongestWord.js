ce774ddb68666a38e679e608bbca4a018a594156
Thein Naing <theinnaing0908@gmail.com>
2023-09-13 15:33:43 +0800

    Update LongestWord.js

--- LongestWord.js
+++ LongestWord.js
@@ -3,20 +3,19 @@ Have the function LongestWord(sen) take the sen parameter being passed and retur
 If there are two or more words that are the same length, return the first word from the string with that length. 
 Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567" */
 
-
 function LongestWord(sen) { 
 
-  //1. use regex string.match method to return only characters a through z and 0 through 9, stripping away punctuation.
-let arr = sen.match(/[a-z]+/gi);
+  /* 1. use regex string.match method to return only characters a through z and 0 through 9, 
+  stripping away punctuation. 
 
+  2. use the arr.reduce method with (a, b) parameter which will compare each element with next element.
 
-  //2. sort the arr with a function(a, b) parameter which will compare each element with next element.
-sortedArr = arr.sort(function((a, b) => {
-  b.length > a.length 
-});
+  3. Assuming there are two or more words that are the same length,
+   return the first word from the string with that length. */
 
-  //3. Assuming there are two or more words that are the same length, return the first word from the string with that length.
-return sortedArr[0];
+return sen.match(/[a-z0-9]+/gi).reduce((a,b) => a.length >= b.length ? a : b);
+
+}
 
 // keep this function call here 
 LongestWord(readline());
