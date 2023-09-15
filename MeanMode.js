fce30b8b578b7bb4ae8c712643de854203c6869c
Thein Naing <theinnaing0908@gmail.com>
2023-09-15 12:32:19 +0800

    Update MeanMode.js

--- MeanMode.js
+++ MeanMode.js
@@ -4,11 +4,9 @@ Have the function MeanMode(arr) take the array of numbers stored in arr and retu
 0 if they don't equal each other (ie. [5, 3, 3, 3, 1] should return 1 because the mode (3) equals the mean (3)).
 The array will not be empty, will only contain positive integers, and will not contain more than one mode. */
 
-
 function MeanMode(arr) { 
 
   // code goes here  
-
   // 1. use arr.reduce method to find mean. 
   
   let mean = arr.reduce((a,b) => a + b) / arr.length;
@@ -21,9 +19,12 @@ function MeanMode(arr) {
   to find the frequencies of occurence in the array.
 
    3A. if found that occurence is more than one time, increment current element value
-    or set it to one if not found. take note using || operator for found and not found values.*/
+    or set it to one if not found. take note using || operator for found and not found values.
+    
+    3B. obj[el] = (obj[el] ) + 1) ===  if found that occurence is more than one time.
+    3C. obj[el] = (0) + 1)  ===   if  occurence  is not found. */
   
-  arr.forEach(el => obj[el] = (obj[el] || 0) + 1);
+  arr.forEach(el => obj[el] = (obj[el] || 0) + 1); 
 
   /*4. then compare frequencies of each every element in modeObj 
   by using arr.reduce method and ternary operator. */
