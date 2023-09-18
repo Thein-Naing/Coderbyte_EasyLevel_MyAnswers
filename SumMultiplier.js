4b855e6cd0340971b29b41f365e52cc48b62c42c
Thein Naing <theinnaing0908@gmail.com>
2023-09-18 10:50:48 +0800

    Create SumMultiplier.js

---
+++ SumMultiplier.js
@@ -0,0 +1,43 @@
+/* Sum Multiplier
+Have the function SumMultiplier(arr) take the array of numbers stored in arr and return the string true 
+if any two numbers can be multiplied so that the answer is greater than double the sum of all the elements in the array. 
+If not, return the string false. For example: if arr is [2, 5, 6, -6, 16, 2, 3, 6, 5, 3]
+then the sum of all these elements is 42 and doubling it is 84. 
+There are two elements in the array, 16 * 6 = 96 and 96 is greater than 84, so your program should return the string true.
+
+Examples
+Input: [2, 2, 2, 2, 4, 1]
+Output: false
+Input: [1, 1, 2, 10, 3, 1, 12]
+Output: true
+
+Tags
+array math fundamentals sequences  */
+
+function SumMultiplier(arr) { 
+
+  // code goes here  
+  
+/*  Method A.*/ 
+//   let sum = arr.reduce((a, b) => ( a +b)) * 2;
+//     for(let i =0; i < arr.length; i++) {
+//       for(let j = i + 1; j < arr.length; j++ ) {
+//         if ((arr[i] * arr[j])  > sum ) {
+//            return true;
+//         }
+//       }
+//     }
+//     return false;
+// }
+
+  /* Method B.*/
+
+  // let sum = arr.reduce((a, b) => ( a +b)) * 2;  
+  // return  arr.sort((a, b) => b -a)[0]  * arr.sort((a, b) => b-a)[1] > sum 
+
+  /* Method C*/
+  
+  return  arr.sort((a, b) => b -a)[0]  * arr.sort((a, b) => b-a)[1] > arr.reduce((a, b) => ( a +b)) * 2; 
+ 
+// keep this function call here 
+console.log(SumMultiplier(readline()));
