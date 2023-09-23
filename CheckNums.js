4eb567030bfd5447d11188dcb9644fbd95057d87
Thein Naing <theinnaing0908@gmail.com>
2023-09-23 20:04:03 +0800

    Update CheckNums.js

--- CheckNums.js
+++ CheckNums.js
@@ -3,19 +3,18 @@ Have the function CheckNums(num1,num2) take both parameters being passed and ret
 if num2 is greater than num1, otherwise return the string false. 
 If the parameter values are equal to each other then return the string -1. */
 
-
 function CheckNums(num1,num2) { 
 
   // code goes here  
-  if (num2 === num1) {
-    return -1;
-  }
-  if (num2 > num1) {
-  return true 
-  } else {
-    return false;
-  }
+
+  // 1. if num2 is greater than num1, return the string true.
+  // 2. if num2 is greater than num1, return the string false. 
+  // 3. If the parameter values are equal to each other ( meaning: num1 = num2) then return the string -1.
+  // 4. use ternary operator and return it.
+
+    return  num2 === num1 ? -1 : num2 > num1 ? true  : false;
+
 }
    
 // keep this function call here 
-console.log(CheckNums(readline()))
+console.log(CheckNums(readline()));
