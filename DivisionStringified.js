e01f10b5bb547698f931c6dbf654c0e1eaaebe0b
Thein Naing <theinnaing0908@gmail.com>
2023-09-23 21:54:44 +0800

    Update DivisionStringified.js

--- DivisionStringified.js
+++ DivisionStringified.js
@@ -14,10 +14,12 @@ function DivisionStringified(num1,num2) {
     3. if result.length > 3  then we have to loop through each every length of result. 
        in this case, use arr.splice method to remove unwanted result, add "," or replace result with ",". 
     4. then rejoin and return result. 
-    5. meaning: by using arr.splice method, if  i % 3 === 0  from backward index of result, ( we declare i-- in for loop)
-       then delete elements will start from index at result.length -1.
+    5. meaning: by using arr.splice method, if  i % 3 === 0,
+       then delete elements will start from index at result.length - i.
        delete count will be 0 .
-       and add "" new element will be right after the delete count of 0. */
+       and add "," new element will be right after the delete count of 0.
+    6. e.g. num1 = 123456789 and num2 = 10000 , result = "12346", if i === 3 then result.length -i = 2 
+            then "," will be added at index 2 of 12346 and 12,346. */
 
 
   let result = Math.round(num1 / num2).toString().split("");
