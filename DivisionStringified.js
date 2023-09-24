9cdbd2ce2df00addc04cc209a1902944fb534a2f
Thein Naing <theinnaing0908@gmail.com>
2023-09-24 09:45:02 +0800

    Update DivisionStringified.js

--- DivisionStringified.js
+++ DivisionStringified.js
@@ -8,7 +8,7 @@ function DivisionStringified(num1,num2) {
 
   // code goes here  
   
- /* 1. convert result of num1 divided by num2 to string using rounded with Math.round method,
+ /* 1. convert result of num1 divided by num2 to ordered list of words string using rounded with Math.round method,
        arr.toString() and arr.split("") methods.
     2. if result.length <= 3, then just simply rejoin and return result.
     3. if result.length > 3  then we have to loop through each every length of result. 
@@ -17,9 +17,9 @@ function DivisionStringified(num1,num2) {
     5. meaning: by using arr.splice method, if  i % 3 === 0 (in for loop, we will check result length of i % 3 === 0 )
                 from starting  i = result.length - 1 and then continue checking backward index of result.)
                 then delete elements will start from index at result.length - i.
-                delete count will be 0 .
-                and add "," new element will be right after the delete count of 0.
-    6. e.g. num1 = 123456789 and num2 = 10000 , result = "12346", if i % 3 === 0, then result.length -i = 2. 
+                delete count will be 0 . since we will not delete any number so we give delete count quantity to 0 in arr.splice method call.
+                we just want to add "," sign. so add "," and new element: "," will be right after the delete count of 0.
+    6. e.g. num1 = 123456789 and num2 = 10000 , result = "12346", if i % 3 === 0, ---> i = 3 or i = 6 or e.t.c. for this case ---> i = 3 ---> result.length -i = 2. 
             here delete count is 0. so "," will be added at index 2 of 12346 and final result become 12,346. */
 
 
