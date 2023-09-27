bbc9d8ca8956f00f3d8f2d345fbb94e758fe363d
Thein Naing <theinnaing0908@gmail.com>
2023-09-27 20:11:53 +0800

    Update AdditivePersistence.js

--- AdditivePersistence.js
+++ AdditivePersistence.js
@@ -4,7 +4,6 @@ Have the function AdditivePersistence(num) take the num parameter being passed w
 return its additive persistence which is the number of times you must add the digits in num until you reach a single digit. 
 For example: if num is 2718 then your program should return 2 because 2 + 7 + 1 + 8 = 18 and 1 + 8 = 9 and you stop at 9.   */
 
-
 function AdditivePersistence(num) { 
 
  // note: if you can solve  MultiplicativePersistence challenge, you can also solve this challenge.
@@ -22,13 +21,16 @@ function AdditivePersistence(num) {
 
 
   num = num.toString().split('').reduce((a,b) => parseInt(a) + parseInt(b), 0);   /*3. specified condition here is we must add the digits in given num
-                                                                     until we reach a single digit. 
-                                                                     so use arr.reduce method to satisfy this condition */
+                                                                                       until we reach a single digit. so use arr.reduce method to satisfy this condition 
+                                                                                       notes:  array.reduce(function(total, currentValue), initialValue) 
+                                                                   total = the initialValue, or the previously returned value of the function. here total = a;
+                                                                   currentValue = The value of the current element. here currentValue = b;
+                                                                   initialValue = A value to be passed to the function as the initial value. here initialValue = 0; */
+  
     count += 1;                                                // 4. if it is true, update the count.
   }
   return count;                                                // 5. return count.
 }
-
    
 // keep this function call here 
 console.log(AdditivePersistence(readline()));
