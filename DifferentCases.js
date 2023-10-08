34dc7a12ea769b5f260dabfaed7eb5aa3788ead5
Thein Naing <theinnaing0908@gmail.com>
2023-10-08 13:01:48 +0800

    Update DifferentCases.js

--- DifferentCases.js
+++ DifferentCases.js
@@ -24,17 +24,17 @@ function DifferentCases(str) {
      we will convert first letter of each word to uppercase by using str.toUppcase() method and the charAt() method which returns the specified character from a string. 
      here is charAt(0) of letters[i] and then use str.slice() method which will extract from index 1 to the end of the string.
      
-  3. Here str.length = 5 === "cats AND*Dogs-are Awesome"
+  3. Here str.length = 5 === "cats AND*Dogs-are Awesome" become cats and dogs are awesome after using str.toLowerCase().replace(/\W/g, " ").split(" ").
     1st iteration: str[0] = str[0].charAt(0).toUpperCase() + str[0].slice(1);
                    str[0] = "cats".charAt(0).toUpperCase()  + "cats".slice(1);
                    str[0] = "C"                            + "ats";
                    str[0] = "Cats";
     2nd iteration: str[1] = str[1].charAt(0).toUpperCase() + str[1].slice(1);
-                   str[1] = "AND".charAt(0).toUpperCase()    + "AND".slice(1);
+                   str[1] = "and".charAt(0).toUpperCase()    + "and".slice(1);
                    str[1] = "A"                            + "nd";
                    str[1] = "And";
     3rd iteration: str[2] = str[2].charAt(0).toUpperCase()   + str[2].slice(1);
-                   str[2] = "Dogs".charAt(0).toUpperCase() + "dogs".slice(1);
+                   str[2] = "dogs".charAt(0).toUpperCase() + "dogs".slice(1);
                    str[2] = "D"                              + "ogs";
                    str[2] = "Dogs";
     4th iteration: str[3] = str[3].charAt(0).toUpperCase() + str[3].slice(1);
@@ -42,7 +42,7 @@ function DifferentCases(str) {
                    str[3] = "A"                            + "re";
                    str[3] = "Are";
     5th iteration: str[4] = str[4].charAt(0).toUpperCase() + str[4].slice(1);
-                   str[4] = "Awesome".charAt(0).toUpperCase() + "Awesome".slice(1);
+                   str[4] = "awesome".charAt(0).toUpperCase() + "awesome".slice(1);
                    str[4] = "A"                           + "wesome";
                    str[4] = "Awesome";                                                         
     End of the For Loop.
