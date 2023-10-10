65c91115fc73481dcd489c49984150c347020a78
Thein Naing <theinnaing0908@gmail.com>
2023-10-10 19:05:16 +0800

    Update StringMerger.js

--- StringMerger.js
+++ StringMerger.js
@@ -18,11 +18,13 @@ function StringMerge(str) {
 
   // code goes here  
 
-  let strA = str.split("*")[0] // 1. divide the given string into an ordered array of letters using arr.split(" * ") and assign str[0].
-  let strB = str.split("*")[1] // 2. divide the given string into an ordered array of letters using arr.split(" * ") and assign str[1].
-  let strC = ""                // 3. initiall result string shall be empty string and eventually alphanumeric characters will be added alternatively from strA and strB.
+  let strA = str.split("*")[0]     /* 1. divide the given string into an ordered array of letters using arr.split(" * ") and assign str[0].
+                                     after spliting "123hg*aaabb" become 123hg aaabb and we will assign strA = 123hg.  */
+  let strB = str.split("*")[1]     /* 2. divide the given string into an ordered array of letters using arr.split(" * ") and assign str[1].
+                                         after spliting "123hg*aaabb" , it become 123hg aaabb and we will assign strB = aaabb.  */
+  let strC = ""                    // 3. initiall result string shall be empty string and eventually alphanumeric characters will be added alternatively from strA and strB.
   
-  for (let i = 0; i < strA.length; i++) {   // 4. initiale For Loop and i shall be strated counting from strA.
+  for (let i = 0; i < strA.length; i++) {   // 4. initiate For Loop and value of i shall be strated counting from strA.
       strC += (strA[i] + strB[i])           // 5. then adding alphanumeric characters to new str alternatively from strA and strB and assign to strC.
    }
    return strC;                             // 6. return strC.
