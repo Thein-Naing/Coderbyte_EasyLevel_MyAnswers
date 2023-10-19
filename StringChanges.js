adafa9ae81b3b426520af83365d46eaaab50a3ba
Thein Naing <theinnaing0908@gmail.com>
2023-10-19 14:17:05 +0800

    Update StringChanges.js

--- StringChanges.js
+++ StringChanges.js
@@ -19,11 +19,11 @@ function StringChanges(str) {
   // code goes here  
   str = str.split("");                           // 1. Divide the given string into an ordered array of characters using arr.split(" ") method.
   for (let i = 0; i < str.length; i++) {         // 2. Use For Loop to iterate through each every characters of given str to check "M" or "N" is in the str.
-      if(str[i] === "M") {                       /* 3. Given Statement: Whenever a capital M is encountered, duplicate the previous character (then remove the M) 
-                                                       return value of str[i] is equal to value of str[i-1], Meaning: duplicate the previous character(str[i-1]. */
+      if(str[i] === "M") {                       /* 3. Given Statement: Whenever a capital M is encountered, duplicate the previous character (then remove the M). 
+                                                       We will return value of str[i] is equal to value of str[i-1], Meaning: duplicate the previous character(str[i-1]. */
         str[i] = str[i-1]
-    } else if (str[i] === "N") {                 /* 4. Given Statement: whenever a capital N is encountered remove the next character from the string (then remove the N) 
-                                                       return value of str[i] and str[i+1] as empty string, Meaning: there is no more str[i] and str[i+1] in str */
+    } else if (str[i] === "N") {                 /* 4. Given Statement: whenever a capital N is encountered remove the next character from the string (then remove the N). 
+                                                       We will return value of str[i] and str[i+1] as empty string, Meaning: there is no more str[i] and str[i+1] in str */
         str[i] = str[i+1] = "";       
       }
     }
