c5e8b1d332741650d894691bc082bee6a8499ce3
Thein Naing <theinnaing0908@gmail.com>
2023-10-23 18:32:31 +0800

    Update AlphabetSearching.js

--- AlphabetSearching.js
+++ AlphabetSearching.js
@@ -15,7 +15,7 @@ string manipulation searching */
 
 function AlphabetSearching(str) { 
    str = str.match(/[a-z]/gi); 
-     return str.filter((current, index) => str.indexOf(current) === index).length === 26 ? true : false;
+     return str.filter((current, index) => str.lastIndexOf(current) === index).length === 26 ? true : false;
   
 // keep this function call here 
 console.log(AlphabetSearching(readline()))
@@ -27,7 +27,7 @@ function AlphabetSearching(str) {
 
   // code goes here  
     str = str.match(/[a-z]/gi);                                              // 1. Check each every character in given str exist in English alphabet using arr.match() method with regex( /[a-z]/).
-    str = str.filter((current, index) => str.indexOf(current) === index);    /* 2. Use arr.filter() method to filter each every character with index and make sure index number of current character will be current index.
+    str = str.filter((current, index) => str.indexOf(current) === index);    /* 2. Use arr.filter() method to filter each every character with index and make sure lastIndexOf number of current character will be current index.
                                                                                    Meaning: We will count only one time for repeated characters  */
    
     return str.length === 26 ? true : false                                  // 3. If updated str length  is equal to 26, then true otherwise false and return str.
