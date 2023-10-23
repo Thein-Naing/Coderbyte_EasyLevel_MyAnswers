6e4c1f5416e7d7673fea483ca952ce799cc50c2b
Thein Naing <theinnaing0908@gmail.com>
2023-10-23 14:10:34 +0800

    Update NonRepeatingCharacter.js

--- NonRepeatingCharacter.js
+++ NonRepeatingCharacter.js
@@ -16,17 +16,17 @@ searching hash table Amazon Microsoft */
 function NonrepeatingCharacter(str) { 
 
   // code goes here  
- for(let i = 0; i < str.length; i++) {
+ for(let i = 0; i < str.length; i++) {                                                   // 1. Loop through each every character of given str to check if a character is repeated.
      
-    if (str.indexOf(str[i]) === i && str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
-       return str[i];
+    if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) {                               /* 2. Use str.indexOf() and str.lastIndexOf() methods to determine if an index is repeated. 
+                                                                                               Meaning, if the first occurrence of the character is also the last occurrence, 
+                                                                                               then (you know) it doesn't repeat. If not true, then it does repeat.*/
+       return str[i];                                                                    // 3. If it doesn't repeat , return str[i].
           }
       }
-  return -1;
+  return -1;                                                                             // 4. If not true , then it does repeat and return -1.
   } 
     
-
-  
    
 // keep this function call here 
 console.log(NonrepeatingCharacter(readline()));
