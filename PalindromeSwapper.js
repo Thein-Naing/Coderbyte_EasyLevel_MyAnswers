1e7bb594e193f9f312d333c8971e73bac11b3a5b
Thein Naing <theinnaing0908@gmail.com>
2023-10-26 22:19:24 +0800

    Update PalindromeSwapper.js

--- PalindromeSwapper.js
+++ PalindromeSwapper.js
@@ -11,14 +11,14 @@ function PalindromeSwapper(str) {
 
  	for(let i = 0; i < str.length; i++) {
     let word = str.slice(0, i) + str[i + 1] + str[i] + str.slice(i + 2);    /* 1. Create new word by swapping two adjacent characters in the string. 
-                                                                                  Meaning: Create a palindrome b swapping the second and third characters of given str ----> str[i] + str[i+1] to str[i + 1] + str[i].
-                                                                                  (Given Statement (Given Statement: if a palindrome can be created by swapping two adjacent characters in the string.) */
- 		if (word === word.split('').reverse().join('')){                        /* 2. If created word  can be converted to palindrime , then return this word.
-			return word;                                                                (Given Statement: If it is possible to create a palindrome, then your program should return the palindrome) */
+                                                                                  Meaning: Create a palindrome by swapping the second and third characters of given str ----> str[i] + str[i+1] to str[i + 1] + str[i].
+                                                                                  (Given Statemen: if a palindrome can be created by swapping two adjacent characters in the string.) */
+ 		if (word === word.split('').reverse().join('')) {            /* 2. If created word can be converted to palindrime, then return this word.
+			return word;                                               (Given Statement: If it is possible to create a palindrome, then your program should return the palindrome) */
 		}
 	}	
 	
-	return -1;                                                               // 3. if not then return the string -1.
+	return - 1;                                                          // 3. if not then return the string - 1.
 }
    
 // keep this function call here 
