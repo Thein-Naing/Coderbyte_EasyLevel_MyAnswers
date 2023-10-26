d13478fc1f23381a174d143f42967d4b2709c8a0
Thein Naing <theinnaing0908@gmail.com>
2023-10-26 22:20:17 +0800

    Update and rename NextPelinDrome.js to NextPelindrome.js

--- NextPelindrome.js
+++ NextPelindrome.js
@@ -15,8 +15,8 @@ function NextPalindrome(num) {
   // code goes here  
 
  let word = (num + 1)                                                              // 1. Define a variable called word and set its initial value of next palindrome num shall be  num + 1. 
-    while (word.toString().split('').reverse().join('') !== word.toString())       // 2. Specify true or false for while loop.
-        word++;                                                                    // 3. If condition is met, then update word.
+    while (word.toString().split('').reverse().join('') !== word.toString())       // 2. Specify true or false condition for while loop.
+        word++;                                                                    // 3. If desired condition is met, then update word.
     return word;                                                                   // 4. Return word.
 
 }
