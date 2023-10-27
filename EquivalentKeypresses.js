f868d05be8efe1467608a7ee23f3d43648a1f5f4
Thein Naing <theinnaing0908@gmail.com>
2023-10-27 14:57:15 +0800

    Update EquivalentKeypresses.js

--- EquivalentKeypresses.js
+++ EquivalentKeypresses.js
@@ -16,9 +16,16 @@ array Google */
 function EquivalentKeypresses(strArr) { 
 
   // code goes here  
-  const arr1 = strArr[0].split(",");
-  const arr2 = strArr[1].split(",");
 
+    /* 1. Extract letters arr1 and arr2 from given strArr using arr.split(",") method which converts a string to array.
+       2. Define stacks to hold letters from arr1 and arr2.
+       3. Use arr.map() method with single argument(character = char) and we will modify each letter in arr1 and arr2. 
+       4. If character is equal to "-B" then we will delete thelast letter of arr otherwis ewe will add this character to stack.
+       5. Make if else statement by chaining arr.map() call with str.pop(), str.push() and ternary operator.
+       6. Rejoin anfd retrun letters stacks. If strA and strB are equal, the return true, otherwise return false. */
+  
+  const arr1 = strArr[0].split(",");      // console.log(arr1);  // ['a', 'b', 'c', 'd']
+  const arr2 = strArr[1].split(",");      // console.log(arr2); // ['a', 'b', 'c', 'd', '-B', 'd']
   const  strA = [];
   const  strB = [];
 
