efb023c12be659737457047a4a69dbd0a6170189
Thein Naing <theinnaing0908@gmail.com>
2023-11-08 16:07:04 +0800

    Create ProductDigits.js

---
+++ ProductDigits.js
@@ -0,0 +1,31 @@
+/*Product Digits
+Have the function ProductDigits(num) take the num parameter being passed which will be a positive integer, and determine the least amount of digits you need to multiply to produce it. 
+For example: if num is 24 then you can multiply 8 by 3 which produces 24, so your program should return 2 because there is a total of only 2 digits that are needed.
+Another example: if num is 90, you can multiply 10 * 9, so in this case your program should output 3 because you cannot reach 90 without using a total of 3 digits in your multiplication.
+Examples
+Input: 6
+Output: 2
+Input: 23
+Output: 3
+
+Tags
+math fundamentals searching */
+
+
+function ProductDigits(num) { 
+
+  // code go here
+  let stack = [];
+    for(let i = 0; i <= num; i++) {
+       for (j = 0; j <= num; j++) {
+         if (i * j === num) 
+         stack.push(parseInt(i.toString().length) + parseInt(j.toString().length));      
+         
+       }
+    }
+ return stack.sort((a,b)=> a - b)[0];
+    
+}
+   
+// keep this function call here 
+console.log(ProductDigits(readline()));
