b84d201cbbebdeacbb20b87fbb3db27f6f5ae0d0
Thein Naing <theinnaing0908@gmail.com>
2023-11-15 12:49:35 +0800

    Create Number Stream.js

---
+++ Number Stream.js
@@ -0,0 +1,31 @@
+/* Number Stream
+easy 
+
+Have the function NumberStream(str) take the str parameter being passed which will contain the numbers 2 through 9, and determine if there is a consecutive stream of digits of at least N length where N is the actual digit value. If so, return the string true, otherwise return the string false. For example: if str is "6539923335" then your program should return the string true because there is a consecutive stream of 3's of length 3. The input string will always contain at least one digit.
+Examples
+Input: "5556293383563665"
+Output: false
+Input: "5788888888882339999"
+Output: true
+Tags
+string manipulation searching data science back-end Amazon */
+
+function NumberStream(str) { 
+
+  // code goes here  
+
+let digits = [];
+    for (let i = 2; i <= 9; i++) {
+        digits.push(String(i).repeat(i));    
+    for (let digit of digits) {
+        if (str.includes(digit))           
+            return true;
+        }
+
+    }
+    return false;
+
+}
+   
+// keep this function call here 
+console.log(NumberStream(readline()));
